0d4f5b784c8b544b43986200a252ee894c97f09d
Stephy Raju <stephyraju@Stephys-MacBook-Air.local>
2020-10-20 11:04:41 +0100

    #S07: Created teaminfo model

--- teaminfo/models.py
+++ teaminfo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Hackteam(models.Model):
+    team_name = models.CharField(max_length=250)
+    participants = models.ManyToManyField(User,
+                                          related_name='team_participants')
+def __str__(self):
+        return self.team_name
